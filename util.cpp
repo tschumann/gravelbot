5a0798ea86eccfabe0f9ba06a692bf78b6a2622d
tschumann <schumann.tom@gmail.com>
2017-12-26 14:02:21 +1000

    Tidied up the code a bit.

--- util.cpp
+++ util.cpp
@@ -14,7 +14,7 @@ CBaseEntity* GetBaseEntity( edict_t *pEntity )
 {
 	if( !pEntity || pEntity->IsFree() )
 	{
-		return NULL;
+		return nullptr;
 	}
 
 	CBaseEntity *pent = pEntity->GetUnknown()->GetBaseEntity();
@@ -26,16 +26,19 @@ BotBasePlayer *CreateBasePlayer( edict_t *pEdict )
 {
 	switch( engine->GetAppID() )
 	{
-	case Game::HL2DM_APPID:
-		return new hl2dm::CBasePlayer( GetBaseEntity( pEdict ) );
-		break;
 	case Game::DOD_APPID:
 		return new dod::CBasePlayer( GetBaseEntity( pEdict ) );
 		break;
-	case Game::BMS_APPID:
+	case Game::HL2DM_APPID:
+		return new hl2dm::CBasePlayer( GetBaseEntity( pEdict ) );
 		break;
+	case Game::PORTAL2_APPID:
+	case Game::MM_APPID:
+	case Game::INSURGENCY_APPID:
+	case Game::EYE_APPID:
+	case Game::BMS_APPID:
 	default:
 		Error( "Unsupported appid %d\n", engine->GetAppID() );
-		return NULL;
+		return nullptr;
 	}
 }
